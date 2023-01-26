0f29b3915dc09c7b7fef8508b6af5041fdb66f36
Aleksandr60rus <113985770+Aleksandr60rus@users.noreply.github.com>
2023-01-26 19:26:01 +0200

    old

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import Users from "./components/users";
+import ReactDOM from "react-dom/client";
 import "./index.css";
-import "bootstrap/dist/css/bootstrap.css";
-
 import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.css";
+import Users from "./app/components/users";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <Users />
-  </React.StrictMode>,
-  document.getElementById(`root`)
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
