db56b4baf75977c5ad0bc105328d06a3c99e55a2
Aleksandr60rus <113985770+Aleksandr60rus@users.noreply.github.com>
2023-02-06 22:26:39 +0200

    Chfnged4

--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -1,29 +1,63 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import PropTypes from "prop-types";
 import { paginate } from "../utils/paginate";
 import Pagination from "./pagination";
 import User from "./user";
+import api from "../api";
 import GroupList from "./groupList";
 const Users = ({ users: allUsers, ...rest }) => {
     const [currentPage, setCurrentPage] = useState(1);
+    const [professions,setProfession] = useState (api.professions.fetchAll());
+    const [selectedProf,setSelectedProf] = useState ();
     const count = allUsers.length;
     const pageSize = 4;
+    useEffect(()=>{
+        api.professions.fetchAll().then((data)=>
+         setProfession(
+         data
+            )
+        );
+         }, []);
+
+    const handleProfessionSelect = (item) => { 
+        setSelectedProf(item);
+    };
+    console.log(professions);
 
     const handlePageChange = (pageIndex) => {
         setCurrentPage(pageIndex);
-        console.log("page: ", pageIndex);
     };
-    const usersCrop = paginate(allUsers, currentPage, pageSize);
+    const filteredUsers = selectedProf 
+    ? allUsers.filter
+    ((user)=> user.profession === selectedProf) 
+    : allUsers;
+    
+    const usersCrop = paginate(filteredUsers, currentPage, pageSize);
+    const clearFilter = () => {
+  setSelectedProf();
+    };
     return (
-        <>
-        < GroupList />
+        <> {professions && (
+            <>
+        < GroupList 
+        selectedItem = {selectedProf}
+        items={professions}
+         onItemSelect = {handleProfessionSelect}
+
+         valueProperty="_id"
+         contentProperty="name"
+         /> 
+      <button className= "btn btn-secondary mt-2" 
+      onClick = {clearFilter}>Очистить</button>
+         </>
+        )}
             {count > 0 && (
                 <table className="table">
                     <thead>
                         <tr>
                             <th scope="col">Имя</th>
                             <th scope="col">Качества</th>
-                            <th scope="col">Провфессия</th>
+                            <th scope="col">Профессия</th>
                             <th scope="col">Встретился, раз</th>
                             <th scope="col">Оценка</th>
                             <th scope="col">Избранное</th>
