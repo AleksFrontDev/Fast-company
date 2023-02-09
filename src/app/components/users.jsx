9c88a235eeca8ed0331d339fb20f9ce31226a8d1
Aleksandr60rus <113985770+Aleksandr60rus@users.noreply.github.com>
2023-02-09 20:15:24 +0200

    Sort

--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -7,11 +7,13 @@ import api from "../api";
 import GroupList from "./groupList";
 import SearchStatus from "./searchStatus";
 import UserTable from "./usersTable";
+import _ from "lodash";
 const Users = ({ users: allUsers, ...rest }) => {
     const [currentPage, setCurrentPage] = useState(1);
     const [professions,setProfession] = useState (api.professions.fetchAll());
     const [selectedProf,setSelectedProf] = useState ();
-    const pageSize = 2;
+    const [sortBy,setSortBy] = useState ({iter:"name",order:'asc'});
+    const pageSize = 8;
     useEffect(()=>{
         api.professions.fetchAll().then((data)=>
          setProfession(
@@ -32,14 +34,23 @@ setCurrentPage(1);
         setCurrentPage(pageIndex);
     };
     const handleSort = (item) =>{
-console.log("item");
+        if(sortBy.iter===item) {
+            setSortBy((prevState)=>({...prevState,
+                order:prevState.order === "asc" ? "desc" : "asc"
+            }));
+        } else {
+            setSortBy({iter:item,order:'asc'});
+        };
     };
     const filteredUsers = selectedProf 
     ? allUsers.filter
     ((user)=> user.profession === selectedProf) 
     : allUsers;
     const count = filteredUsers.length;
-    const usersCrop = paginate(filteredUsers, currentPage, pageSize);
+
+    const sortedUsers = _.orderBy(filteredUsers,[sortBy.iter],['desc'],sortBy.order);
+
+    const usersCrop = paginate(sortedUsers, currentPage, pageSize);
     const clearFilter = () => {
   setSelectedProf();
     };
